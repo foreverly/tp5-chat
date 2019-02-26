84b8c99b8ff463a0754786e058724ad45981bc76
Bruce <185330767@qq.com>
2019-02-26 11:23:46 +0800

    test

--- application/index/controller/WechatController.php
+++ application/index/controller/WechatController.php
@@ -42,8 +42,44 @@ class WechatController extends Controller
 		$wechatObj->valid();
     }
 
+    // public function responseMsg()
+    // {
+    //     (new WechatCallbackApi)->responseMsg();
+    // }
     public function responseMsg()
     {
-        (new WechatCallbackApi)->responseMsg();
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+        //extract post data
+        if (!empty($postStr)){
+                
+                $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $keyword = trim($postObj->Content);
+                $time = time();
+                $textTpl = "<xml>
+                            <ToUserName><![CDATA[%s]]></ToUserName>
+                            <FromUserName><![CDATA[%s]]></FromUserName>
+                            <CreateTime>%s</CreateTime>
+                            <MsgType><![CDATA[%s]]></MsgType>
+                            <Content><![CDATA[%s]]></Content>
+                            <FuncFlag>0</FuncFlag>
+                            </xml>";             
+                if(!empty( $keyword ))
+                {
+                    $msgType = "text";
+                    $contentStr = "感谢关注!";
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                    echo $resultStr;
+                }else{
+                    echo "Input something...";
+                }
+
+        }else {
+            echo "";
+            exit;
+        }
     }
 }
