6b65fd29a5b07b420364b31323e958461ceb582a
foreverly <bruce_1110@163.com>
2019-05-22 11:26:58 +0800

    get ip

--- application/index/controller/Common.php
+++ application/index/controller/Common.php
@@ -20,8 +20,10 @@ class Common extends Controller
 
     public function _initialize()
     {
-        new \PDO('mysql:host=localhost;port=3306;dbname=chat;charset=utf8', 'root', 'root', null);
-        die;
+        $config = config('database');
+        $config['hostname'] = $this->getIp();
+        \think\Config::set('database', $config);
+
         parent::_initialize();
         $this->checkLogin();
         
@@ -34,6 +36,29 @@ class Common extends Controller
             'user_info' => $this->userInfo
         ]);
 
+    }
+
+    /**
+     * 获取ip地址
+     **/
+    public  static function getIp() {
+        static $ip = '';
+        $ip = $_SERVER['REMOTE_ADDR'];
+
+        if(isset($_SERVER['HTTP_CDN_SRC_IP'])) {
+            $ip = $_SERVER['HTTP_CDN_SRC_IP'];
+        } elseif (isset($_SERVER['HTTP_CLIENT_IP']) && preg_match('/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', $_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif(isset($_SERVER['HTTP_X_FORWARDED_FOR']) AND preg_match_all('#\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}#s', $_SERVER['HTTP_X_FORWARDED_FOR'], $matches)) {
+            foreach ($matches[0] AS $xip) {
+              if (!preg_match('#^(10|172\.16|192\.168)\.#', $xip)) {
+                $ip = $xip;
+                break;
+              }
+            }
+        }
+
+        return $ip;
     }
 
     //
