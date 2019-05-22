9a10dc289ff7b27ef77197892473471ef9328eaf
foreverly <bruce_1110@163.com>
2019-05-22 11:31:37 +0800

    get ip

--- application/index/controller/Common.php
+++ application/index/controller/Common.php
@@ -42,23 +42,7 @@ class Common extends Controller
      * 获取ip地址
      **/
     public  static function getIp() {
-        static $ip = '';
-        $ip = $_SERVER['REMOTE_ADDR'];
-
-        if(isset($_SERVER['HTTP_CDN_SRC_IP'])) {
-            $ip = $_SERVER['HTTP_CDN_SRC_IP'];
-        } elseif (isset($_SERVER['HTTP_CLIENT_IP']) && preg_match('/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', $_SERVER['HTTP_CLIENT_IP'])) {
-            $ip = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif(isset($_SERVER['HTTP_X_FORWARDED_FOR']) AND preg_match_all('#\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}#s', $_SERVER['HTTP_X_FORWARDED_FOR'], $matches)) {
-            foreach ($matches[0] AS $xip) {
-              if (!preg_match('#^(10|172\.16|192\.168)\.#', $xip)) {
-                $ip = $xip;
-                break;
-              }
-            }
-        }
-
-        return $ip;
+        return $_SERVER['HTTP_X_FORWARDED_HOST'] ?? ($_SERVER['HTTP_HOST') ?? '');
     }
 
     //
